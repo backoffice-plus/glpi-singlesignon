c6372133e7b4b8257a23a357d42b71101bb6fe27
Julian Haupt <julian@haupt.media>
2023-09-07 13:56:47 +0200

    increase version number to 1.3.6

--- setup.php
+++ setup.php
@@ -25,7 +25,7 @@
  * ---------------------------------------------------------------------
  */
 
-define('PLUGIN_SINGLESIGNON_VERSION', '1.3.5');
+define('PLUGIN_SINGLESIGNON_VERSION', '1.3.6');
 
 $folder = basename(dirname(__FILE__));
 
